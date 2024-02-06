4e01a7c81aa78a89f7a238687ba8f83b7a9b0a55
Leonard <remon.w.dev@gmail.com>
2024-02-06 17:33:15 +0100

    basic graphics

---
+++ include/main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+typedef struct runtimeSettings runtimeSettings;
+
+
+/**
+ * init raylib and other stuff
+ *
+ */
+void init(runtimeSettings* sett);
--- src/main.c
+++ src/main.c
@@ -1,5 +1,51 @@
-#include <stdio.h>
+#include "main.h"
+
+#include <raylib.h>
+
+struct runtimeSettings {
+	Camera2D camera;
+	unsigned screenWidth;
+	unsigned screenHeight;
+};
+
+void init(runtimeSettings *sett) {
+
+	auto sh = GetScreenHeight();
+	auto sw = GetScreenWidth();
+
+	SetTraceLogLevel(LOG_ALL);
+	// setup the window with a name
+	InitWindow(sw, sh, "Slime Light");
+	// ToggleFullscreen();
+
+	sett->camera = (Camera2D){0};
+
+	// try to enable vsync and 60fps still some screen tearing
+	SetWindowState(FLAG_VSYNC_HINT);
+	SetTargetFPS(60);
+
+	// Lock the cursor at the center of the screen and hide it
+	HideCursor(); // Disable also hides
+	DisableCursor();
+}
 
 int main() {
-	printf("Hello mothafuckers");
+
+	runtimeSettings settings;
+
+	init(&settings);
+
+	while (!WindowShouldClose()) {
+
+		ClearBackground(BLACK);
+
+		BeginDrawing();
+		{
+			DrawFPS(10, 10);
+			DrawRectangle(100, 100, 500, 800, WHITE);
+		}
+		EndDrawing();
+	}
+
+	CloseWindow();
 }
